ddde618b55fdc4f13621c331db67f0f565db79b6
Xiaonan Shen <s@sxn.dev>
2020-05-25 02:57:41 -0700

    Add build

--- scipy/build.sh
+++ scipy/build.sh
@@ -2,38 +2,33 @@
 
 set -ex
 
-# Test
+# Install dependencies
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    build-essential gfortran \
+    curl ca-certificate \
+    libopenblas-dev python3-dev python3-numpy python3-setuptools python3-wheel
+
+# Download source code
 VERSION=`cat VERSION`
-cat "test" > scipy-${RELEASE}-cp36-cp36m-linux_aarch64.whl
+URL=https://github.com/scipy/scipy/releases/download/v${VERSUIB}/scipy-${VERSION}.tar.gz
+curl -L $URL -o scipy.tar.gz
+tar -zxvf scipy.tar.gz
+mv scipy-* scipy
+cd scipy
 
-# # Install dependencies
-# apt-get update
-# DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-#     build-essential gfortran \
-#     curl ca-certificate \
-#     libopenblas-dev python3-dev python3-numpy python3-setuptools python3-wheel
+# Setup library
+LIB_PATH=/usr/lib/aarch64-linux-gnu
+export BLAS=${LIB_PATH}/libblas.so
+export LAPACK=${LIB_PATH}/liblapack.so
+export ATLAS=${LIB_PATH}/libatlas.so
 
-# # Download source code
-# cd /build
-# VERSION=`cat VERSION`
-# URL=https://github.com/scipy/scipy/releases/download/v${VERSUIB}/scipy-${VERSION}.tar.gz
-# curl -L $URL -o scipy.tar.gz
-# tar -zxvf scipy.tar.gz
-# mv scipy-* scipy
-# cd scipy
+# Multi core build
+export NPY_NUM_BUILD_JOBS=`nproc --all`
 
-# # Setup library
-# LIB_PATH=/usr/lib/aarch64-linux-gnu
-# export BLAS=${LIB_PATH}/libblas.so
-# export LAPACK=${LIB_PATH}/liblapack.so
-# export ATLAS=${LIB_PATH}/libatlas.so
+python3 setup.py bdist_wheel
 
-# # Multi core build
-# export NPY_NUM_BUILD_JOBS=`nproc --all`
-
-# python3 setup.py bdist_wheel
-
-# # Move out wheel and cleanup
-# mv dist/*.whl ../
-# cd ../
-# rm -rf scipy
+# Move out wheel and cleanup
+mv dist/*.whl ../
+cd ../
+rm -rf scipy
